a3d7ade1165823349659ea69e172bb2bfd9b24b0
Mariana Beilune Abad <maryh.abad@gmail.com>
2025-01-25 07:43:44 -0300

    phone working

---
+++ Assets/Scripts/TelephoneManager.cs
@@ -0,0 +1,70 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelephoneManager : MonoBehaviour
+{
+    public AudioSource phoneSound;
+    public Slider slider;
+    public TextMeshProUGUI callerName;
+    public TextMeshProUGUI timer;
+
+    private float callTime;
+    private bool isCallActive;
+
+    void Start()
+    {
+        if (phoneSound == null)
+        {
+            Debug.LogError("AudioSource component is not assigned.");
+        }
+        else
+        {
+            phoneSound.Play();
+        }
+
+        if (timer == null)
+        {
+            Debug.LogError("TextMeshProUGUI component for timer is not assigned.");
+        }
+        else
+        {
+            timer.enabled = false;
+        }
+
+        callTime = 0f;
+        isCallActive = false;
+    }
+
+    void Update()
+    {
+        if (slider.value == 1 && !isCallActive)
+        {
+            if (phoneSound != null)
+            {
+                phoneSound.Stop();
+            }
+
+            if (timer != null)
+            {
+                timer.enabled = true;
+            }
+
+            isCallActive = true;
+        }
+
+        if (isCallActive)
+        {
+            callTime += Time.deltaTime;
+            UpdateTimerText();
+            slider.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(callTime / 60F);
+        int seconds = Mathf.FloorToInt(callTime % 60F);
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
