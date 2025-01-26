5e6c2ada3a5d4394611cf5967970d932c0046798
Mariana Beilune Abad <maryh.abad@gmail.com>
2025-01-26 09:07:24 -0300

    CrimeScene working

---
+++ Assets/NewspaperDrag.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewspaperDrag : MonoBehaviour
+{
+    private Vector3 lastMousePosition;
+
+    void OnMouseDown()
+    {
+        lastMousePosition = Input.mousePosition;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 delta = Input.mousePosition - lastMousePosition;
+        Vector3 direction = new Vector3(delta.x, delta.y, 0).normalized;
+        transform.Translate(direction * Time.deltaTime * 100);
+
+        lastMousePosition = Input.mousePosition;
+    }
+
+    void Update()
+    {
+        
+    }
+}
