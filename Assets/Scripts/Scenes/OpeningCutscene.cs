07b6efbd4a47949d0cb2567c6c027daa9c8e4f67
Mariana Beilune Abad <maryh.abad@gmail.com>
2025-01-27 15:35:14 -0300

    changed Project Settings for Android build

--- Assets/Scripts/Scenes/OpeningCutscene.cs
+++ Assets/Scripts/Scenes/OpeningCutscene.cs
@@ -7,18 +7,21 @@ public class OpeningCutscene : MonoBehaviour
     public GameObject firstCamera;
     public Camera secondCamera;
 
+    private AudioSource secondCameraAudioSource;
+
     void OnEnable() {
+
+        secondCameraAudioSource = secondCamera.GetComponent<AudioSource>();
         secondCamera.enabled = true;
         firstCamera.SetActive(false);
-        secondCamera.GetComponent<AudioSource>().Play();
-
+        secondCameraAudioSource.Play();
         // Se o áudio tiver terminado, passar para outra cena
         StartCoroutine(ChangeScene());
     }
 
     private IEnumerator ChangeScene()
     {
-        yield return new WaitForSeconds(15f);
+        yield return new WaitForSeconds(secondCameraAudioSource.clip.length);
         SceneManager.LoadScene("Telephone");
     }
 
